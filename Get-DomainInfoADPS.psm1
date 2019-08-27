9cc8f2ca33709773da90da2dab871e15cbc9545f
Scott Sutherland <scott.sutherland@netspi.com>
2019-08-27 12:22:47 -0500

    Added a few things
    Add code from Itamar Mizrahi (@MrAnde7son) -
    https://github.com/MrAnde7son/PowerShell
    Added write spn check
    Added gpo delegation check
    Added ACL dump

--- Get-DomainInfoADPS.psm1
+++ Get-DomainInfoADPS.psm1
@@ -2,7 +2,7 @@
 # -------------------------------------------
 # Function: Get-DomainInfoADPS
 # Author: Scott Sutherland (@_nullbind), NetSPI
-# Version: 1.6
+# Version: 1.7
 # This script can be used to dump interesting
 # information from Active Directory.
 # This function requires the Active Directory 
@@ -690,6 +690,26 @@ function Get-DomainInfoADPS
         # Foreign Users
 
         # Foreign Groups
+
+        # Get gpo delegations
+        Write-Output "Checking for GPO delegations..."
+        $GPODelegationLogons = Get-GPODelegation  -Verbose
+        $GPODelegationLogons_C = $GPODelegationLogons | measure | select count -ExpandProperty count
+        Write-Output " - Found $GPODelegationLogons_C GPO delegations"
+        $GPODelegationLogons | Out-File "$CurrentPath\$AdDomain\$AdDomain-GPO-Delegations.csv" 
+
+        # Get spn write        
+        Write-Output "Checking for users that can write SPNs..."
+        $SPNWriteUsers = Get-WriteSPNUsers  -Verbose
+        $SPNWriteUsers_C = $SPNWriteUsers | measure | select count -ExpandProperty count
+        Write-Output " - Found $SPNWriteUsers_C users can write SPNs"
+        $SPNWriteUsers | Out-File "$CurrentPath\$AdDomain\$AdDomain-Users-WriteSPNs.csv" 
+
+        # Get ACLs      
+        Write-Output "Dumping list of ACLs..."
+        $AdACLs = Invoke-ACLScanner  -Verbose -Domain $AdDomain -OutFile "$CurrentPath\$AdDomain\$AdDomain-Users-ACLs-Final.csv"
+        $AdACLs_C = $AdACLs | measure | select count -ExpandProperty count
+        Write-Output " - Found $AdACLs_C ACLs"         
     }
     End
     {
@@ -703,6 +723,53 @@ function Get-DomainInfoADPS
         #>
     }
 }
+
+
+
+function Get-WriteSPNUsers
+{
+<#
+    Author: Itamar Mizrahi (@MrAnde7son)
+    License: GNU v3
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+<#
+    .SYNOPSIS
+        Searches and prints users with "Write servicePrincipalName" right.
+        Author: Itamar Mizrahi (@MrAnde7son)
+        License: GNU v3
+        Required Dependencies: None
+        Optional Dependencies: None
+    .DESCRIPTION
+
+    .PARAMETER Domain
+     Domain to search, default is current user's domain.
+
+    .EXAMPLE 
+       
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory=$False, Position=0, ValueFromPipeline=$True)]
+        [string]$Domain = $env:USERDOMAIN
+
+    )
+
+    $writeSPNUsers = @()
+    $users = Get-ADUser -Filter * -Server $DOMAIN | select distinguishedname
+    foreach ($user in $users)
+    {
+        $userAD = "AD:\" + $user.distinguishedname
+        $writeSPNUsers += ((Get-Acl $userAD).access | ?{$_.ObjectType -eq "f3a64788-5306-11d1-a9c5-0000f80367c1"} | select IdentityReference).IdentityReference.value
+        #Write-Host $user.distinguishedname
+    }
+    $writeSPNUsers
+}
+
+
 
 function Get-DomainPasswordsGPP
 {
@@ -1067,6 +1134,156 @@ function Get-DomainPasswordsGPP
     {
     }
 }
+
+
+function Get-WriteSPNUsers
+{
+<#
+    Author: Itamar Mizrahi (@MrAnde7son)
+    License: GNU v3
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+<#
+    .SYNOPSIS
+        Searches and prints users with "Write servicePrincipalName" right.
+        Author: Itamar Mizrahi (@MrAnde7son)
+        License: GNU v3
+        Required Dependencies: None
+        Optional Dependencies: None
+    .DESCRIPTION
+
+    .PARAMETER Domain
+     Domain to search, default is current user's domain.
+
+    .EXAMPLE 
+       
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory=$False, Position=0, ValueFromPipeline=$True)]
+        [string]$Domain = $env:USERDOMAIN
+
+    )
+
+    $writeSPNUsers = @()
+    $users = Get-ADUser -Filter * -Server $DOMAIN | select distinguishedname
+    foreach ($user in $users)
+    {
+        $userAD = "AD:\" + $user.distinguishedname
+        $writeSPNUsers += ((Get-Acl $userAD).access | ?{$_.ObjectType -eq "f3a64788-5306-11d1-a9c5-0000f80367c1"} | select IdentityReference).IdentityReference.value
+        #Write-Host $user.distinguishedname
+    }
+    $writeSPNUsers
+}
+
+
+function Invoke-ACLScanner
+{
+<#
+    Author: Itamar Mizrahi (@MrAnde7son)
+    License: GNU v3
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+<#
+    .SYNOPSIS
+        Returns all ACE on every object in the current forest.
+
+        Author: Itamar Mizrahi (@MrAnde7son)
+        License: GNU v3
+        Required Dependencies: None
+        Optional Dependencies: None
+
+    .DESCRIPTION
+
+    .PARAMETER 
+
+    .EXAMPLE 
+        PS C:\> Invoke-ACLScanner
+        Returns all ACE on every object in the current forest.
+       
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter( Position = 0)]
+        [String]
+        $OutFile = '',
+
+        [Parameter( Position = 1)]
+        [String]
+        $Domain
+    )
+
+
+$ACEDictionary = @()
+
+$Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+
+$ExtendedRights = "LDAP://CN=Extended-Rights," + ($Forest.Schema.Name.Split(",")[1..$Forest.Schema.Name.Length] -join ",")
+$Entry = New-Object System.DirectoryServices.DirectoryEntry($ExtendedRights)
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher($Entry)
+$Searcher.SearchScope = "OneLevel"
+$ACEs = $Searcher.FindAll()
+
+foreach ($ace in $ACEs){
+$entry = New-Object PSObject
+$entry | Add-Member NoteProperty 'Name' $ace.Properties.cn
+$entry | Add-Member NoteProperty 'Guid' $ace.Properties.rightsguid
+$ACEDictionary += $entry
+}
+
+$DomainList = @($Forest.Domains | % {$_.GetDirectoryEntry() })
+$Results = @()
+foreach ($Domain in $DomainList){
+    $Searcher = New-Object System.DirectoryServices.DirectorySearcher($Domain)
+    $Filter = "(objectCategory=*)"
+    $Searcher.Filter = $Filter
+    $Searcher.PageSize = 1000
+    $Searcher.SearchScope = "Subtree"
+    $Results += $Searcher.FindAll()
+
+}
+
+foreach ($entry in $Results){
+    $owner = $entry.GetDirectoryEntry().ObjectSecurity.Owner
+    $rights = $entry.GetDirectoryEntry().ObjectSecurity.Access
+    foreach ($right in $rights){
+            $ace = $ACEDictionary | ? { $_.Guid -eq $right.ObjectType}
+            if($ace -eq $null){
+                $ExtendedRightName = ''
+            }
+            else{
+                $ExtendedRightName = $ace.Name[0]
+            }
+            $extendedRight = New-Object psobject
+            $extendedRight | Add-Member NoteProperty 'Owner' $owner
+            $extendedRight | Add-Member NoteProperty 'ActiveDirectoryRights' $right.ActiveDirectoryRights
+            $extendedRight | Add-Member NoteProperty 'InheritanceType' $right.InheritanceType
+            $extendedRight | Add-Member NoteProperty 'ObjectType' $right.ObjectType
+            $extendedRight | Add-Member NoteProperty 'ExtendedRightName' $ExtendedRightName
+            $extendedRight | Add-Member NoteProperty 'InheritedObjectType' $right.InheritedObjectType
+            $extendedRight | Add-Member NoteProperty 'ObjectFlags' $right.ObjectFlags
+            $extendedRight | Add-Member NoteProperty 'AccessControlType' $right.AccessControlType
+            $extendedRight | Add-Member NoteProperty 'IdentityReference' $right.IdentityReference
+            $extendedRight | Add-Member NoteProperty 'IsInherited' $right.IsInherited
+            $extendedRight | Add-Member NoteProperty 'InheritanceFlags' $right.InheritanceFlags
+            $extendedRight | Add-Member NoteProperty 'PropagationFlags' $right.None
+            if($OutFile -ne '') {
+                $extendedRight
+                $extendedRight | Export-Csv -Path $OutFile -Append -force
+                }
+            else {
+                $extendedRight | Export-Csv -Path ".\acl.csv" -Append -force
+            }
+    }
+}
+
+
+}
+# -and $owner -notmatch "A_" -and $owner -notmatch "Domain Admins" -and $owner -notmatch "Enterprise Admins" -and $owner -notmatch "Administrators"
 
 function Import-ADPSModule
 {
