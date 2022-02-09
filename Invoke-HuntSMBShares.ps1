d937d5bf41deb6a7e90ad08886c13a8a3a64b3d0
Scott Sutherland <nullbind@users.noreply.github.com>
2022-02-08 18:06:00 -0600

    Update Invoke-HuntSMBShares.ps1

--- Invoke-HuntSMBShares.ps1
+++ Invoke-HuntSMBShares.ps1
@@ -3,7 +3,7 @@
 #--------------------------------------
 # Author: Scott Sutherland, 2022 NetSPI
 # License: 3-clause BSD
-# Version: v1.4.19
+# Version: v1.5
 # References: This script includes code taken and modified from the open source projects PowerView, Invoke-Ping, and Invoke-Parrell. 
 # TODO: Add export summary csv. Domain, affected shares by type. High risk read, high risk write.
 function Invoke-HuntSMBShares
